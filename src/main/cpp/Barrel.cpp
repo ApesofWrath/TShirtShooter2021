9d394048bc64960c2a73b85a4e15a8981f43e746
ian meamber <ianmeamber@gmail.com>
2021-10-23 11:11:24 -0700

    Made Barrel State Machineand functions inside of Barrel

--- src/main/cpp/Barrel.cpp
+++ src/main/cpp/Barrel.cpp
@@ -5,10 +5,49 @@
 Barrel::Barrel()
 {
     elevation_talon = new WPI_TalonSRX(0);
+}
+
+void Barrel::Init() { 
+    elevation_talon->Set(0);
+}
+
+void Barrel::Down() { 
+    elevation_talon->Set(-0.1);
+}
+
+void Barrel::Up() { 
+    elevation_talon->Set(0.1);
+}
+
+void Barrel::Stop() { 
+    elevation_talon->Set(0);
+}
+
+void Barrel::StateMachine() {
+
+    switch (current_state)
+    {
+        case States::INIT:
+            Init();
+        break;
+
+        case States::DOWN:
+            Down();
+
+        break;
+
+        case States::UP:
+            Up();
+            break;
+        case States::STOP:
+            Stop();
+            break;
+        
+    }
 
 }
 
-float Barrel::GetElevation() {
-    return elevation_talon->GetSelectedSensorPosition(1);
-}
+
+
+
 
