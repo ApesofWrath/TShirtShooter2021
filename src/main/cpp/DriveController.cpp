47b06f042ec1c59c68f99c4230ad8060a3e93705
ian meamber <ianmeamber@gmail.com>
2021-11-06 11:03:18 -0700

    Updated Drive Controller code to work better

--- src/main/cpp/DriveController.cpp
+++ src/main/cpp/DriveController.cpp
@@ -18,60 +18,36 @@ DriveController::DriveController() {
 
 void DriveController::Drive(frc::Joystick *joyThrottle) {
 
-	target_l = 0 * MAX_Y_RPM;
-	target_r = 0 * MAX_Y_RPM;
+	if(joyThrottle->GetPOV() == 0){
+			x_input = 0;
+			y_input = 1; 
+		} else if(joyThrottle->GetPOV() == 45){
+			x_input = 1;
+			y_input = 1;
+		} else if(joyThrottle->GetPOV() == 90){
+			x_input = 1;
+			y_input = 0;
+		} else if(joyThrottle->GetPOV() == 135){
+			x_input = 1;
+			y_input = -1;
+		} else if(joyThrottle->GetPOV() == 180){
+			x_input = 0;
+			y_input = -1;
+		} else if(joyThrottle->GetPOV() == 225){
+			x_input = -1;
+			y_input = -1;
+		} else if(joyThrottle->GetPOV() == 270){
+			x_input = -1;
+			y_input = 0;
+		} else if(joyThrottle->GetPOV() == 315){
+			x_input = -1;
+			y_input = 1;
+		} else{
+			x_input = 0;
+			y_input = 0;
+		}
 
-    /*
-	current_l = (canTalonFrontLeft->GetSensorCollection().GetQuadratureVelocity() / UNITS_PER_ROT) * MINUTE_CONVERSION;
-	current_r = (canTalonFrontRight->GetSensorCollection().GetQuadratureVelocity() / UNITS_PER_ROT) * MINUTE_CONVERSION; //right is negative for forward
-   
-	error_l = target_l - current_l;
-	error_r = target_r + current_r;
-    */
+	canTalonFrontLeft->Set(ControlMode::PercentOutput, x_input);
+	canTalonFrontRight->Set(ControlMode::PercentOutput, -y_input);
 
-	target_yaw_rate = 1.0 * (0) * MAX_YAW_RATE; //left should be positive
-	
-    /*
-    yaw_rate_current = 1.0 * (double) ahrs->GetRawGyroZ()
-			* (double) ((PI) / 180);
-	yaw_error = target_yaw_rate - yaw_rate_current;
-    */
-	
-    target_l = target_l - (target_yaw_rate * (MAX_Y_RPM / MAX_YAW_RATE)); //linear velocities for each side, controlling inputs
-	target_r = target_r + (target_yaw_rate * (MAX_Y_RPM / MAX_YAW_RATE));
-
-    /*
-	d_yaw = yaw_error - yaw_last_error;
-	yaw_output = (k_p_yaw * yaw_error) + (k_d_yaw * d_yaw);
-
-	target_l = target_l - yaw_output; //after using yaw controller
-	target_r = target_r + yaw_output;
-
-	P_l = Kp_d * error_l;
-	P_r = Kp_d * error_r;
-
-	i_l += error_l;
-	i_r += error_r;
-
-	I_l = Ki_d * i_l;
-	I_r = Ki_d * i_r;
-
-	d_r = (error_r - last_error_r);
-	d_l = (error_l - last_error_l);
-
-	D_l = Kd_d * d_l;
-	D_r = Kd_d * d_r;
-    */
-
-	output_l = (Kf_d * target_l); // P_l + I_l + D_l + 
-	output_r = (Kf_d * target_r); // P_r + I_r + D_r +
-
-	canTalonFrontLeft->Set(ControlMode::PercentOutput, -output_l); //backs are slaved to fronts
-	canTalonFrontRight->Set(ControlMode::PercentOutput, output_r);
-
-    /*
-	last_error_l = error_l;
-	last_error_r = error_r;
-	yaw_last_error = yaw_error;
-    */
-}
+	}
--- src/main/include/DriveController.h
+++ src/main/include/DriveController.h
@@ -13,26 +13,13 @@ public:
 				*canTalonBackRight;
 	// AHRS *ahrs;
 
-    double Kf_d = 1.0 / MAX_Y_RPM_ACTUAL; //scale down targets
+    double x_input;
+    double y_input;
 
-    double target_l = 0;
-    double target_r = 0;
-    double target_yaw_rate = 0;
-    double output_l = 0.0;
-    double output_r = 0.0;
-
-    const int CAN_TALON_FRONT_LEFT = 24;
+    const int CAN_TALON_FRONT_LEFT = 24;  
     const int CAN_TALON_BACK_LEFT = 21;
     const int CAN_TALON_FRONT_RIGHT = 27;
     const int CAN_TALON_BACK_RIGHT = 26;
-
-    const double MAX_Y_RPM_ACTUAL = 1325.0; //950
-    const double MAX_Y_RPM = 600.0;
-    const double MAX_ROT_VEL_ACTUAL = 50.0; //35
-    const double MAX_YAW_RATE = (MAX_ROT_VEL_ACTUAL / MAX_Y_RPM_ACTUAL) * MAX_Y_RPM; //scale down from actual rot vel
-
-    const double UNITS_PER_ROT = 4096.0;
-    const double MINUTE_CONVERSION = 600.0; //100 ms to min
 
 
 
